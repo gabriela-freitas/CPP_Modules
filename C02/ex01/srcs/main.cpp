0fd6f8df5772f9fecafdf1af72b3ac7c01fb7ddb
Gabriela Freitas de albuquerque <gafreita@c2r9s3.42lisboa.com>
2022-11-08 22:22:27 +0000

    [C03] finished ex01

--- C02/ex01/srcs/main.cpp
+++ C02/ex01/srcs/main.cpp
@@ -6,12 +6,12 @@
 /*   By: gafreita <gafreita@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 19:29:09 by gafreita          #+#    #+#             */
-/*   Updated: 2022/11/08 20:06:36 by gafreita         ###   ########.fr       */
+/*   Updated: 2022/11/08 20:26:00 by gafreita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-#include "ex00.hpp"
+#include "ex01.hpp"
 #include "Fixed.hpp"
 
 int	main(void)
