d6c3acf07e442d40ea83625f828fec5e676fe663
Gabriela Freitas de albuquerque <gafreita@c2r10s6.42lisboa.com>
2023-06-03 18:59:58 +0100

    [CPP05] ex00 done, starting ex01

---
+++ C05/ex01/includes/Bureaucrat.hpp
@@ -0,0 +1,45 @@
+
+#ifndef __BUREAUCRAT_HPP__
+# define __BUREAUCRAT_HPP__
+#include <iostream>
+#include "ex01.hpp"
+
+class Bureaucrat {
+
+	private:
+		const std::string _name;
+		int _grade;
+
+	public:
+		class myException
+		{
+			public:
+				virtual void message() const {
+						std::cout << RED << "Exception: myException" << ENDC << std::endl;
+				}
+				virtual ~myException() {}
+		};
+
+		class GradeTooHighException: public myException
+		{
+			public:
+				void message() const;
+		};
+
+		class GradeTooLowException: public myException
+		{
+			public:
+				void message() const;
+		};
+
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(const Bureaucrat&);
+		~Bureaucrat();
+		Bureaucrat&	operator= (const Bureaucrat&);
+		std::string getName() const;
+		int getGrade() const;
+		void incrementGrade(int amount);
+		void decrementGrade(int amount);
+};
+std::ostream&	operator<<(std::ostream&, const Bureaucrat&);
+#endif
---
+++ C05/ex01/srcs/Bureaucrat.cpp
@@ -0,0 +1,105 @@
+
+#include "Bureaucrat.hpp"
+#include "ex01.hpp"
+
+Bureaucrat::Bureaucrat(std::string name, int grade):
+	_name(name),
+	_grade(grade)
+{
+	try
+	{
+		if (this->_grade > 150)
+			throw(Bureaucrat::GradeTooLowException());
+		if (this->_grade < 1)
+			throw(Bureaucrat::GradeTooHighException());
+
+	}
+	catch (Bureaucrat::myException &e)
+	{
+		e.message();
+		throw (Bureaucrat::myException());
+	}
+	std::cout << "Bureaucrat default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& param):
+	_name(param.getName()),
+	_grade(param.getGrade())
+{
+	std::cout << "Bureaucrat copy constructor called" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat() {
+	std::cout << "Bureaucrat" << " destroyed" << std::endl;
+}
+
+Bureaucrat& Bureaucrat::operator= (const Bureaucrat& param) {
+	if (this == &param)
+		return (*this);
+	this->_grade = param.getGrade();
+	return (*this);
+}
+
+int Bureaucrat::getGrade() const
+{
+	return (this->_grade);
+}
+
+std::string Bureaucrat::getName() const
+{
+	return (this->_name);
+}
+
+std::ostream& operator<<(std::ostream& s, const Bureaucrat& param)
+{
+	std::cout << param.getName() << ", Bureaucrat grade " << param.getGrade() << std::endl;
+	return (s);
+}
+
+void Bureaucrat::incrementGrade(int amount)
+{
+	try
+	{
+		this->_grade -= amount;
+		if (this->_grade > 150)
+			throw(Bureaucrat::GradeTooLowException());
+		if (this->_grade < 1)
+			throw(Bureaucrat::GradeTooHighException());
+
+	}
+	catch (Bureaucrat::myException &e)
+	{
+		e.message();
+		throw (Bureaucrat::myException());
+	}
+}
+
+void Bureaucrat::decrementGrade(int amount)
+{
+	try
+	{
+		this->_grade += amount;
+		if (this->_grade > 150)
+			throw(Bureaucrat::GradeTooLowException());
+		if (this->_grade < 1)
+			throw(Bureaucrat::GradeTooHighException());
+
+	}
+	catch (Bureaucrat::myException &e)
+	{
+		e.message();
+		throw (Bureaucrat::myException());
+	}
+}
+
+void Bureaucrat::GradeTooHighException::message() const
+{
+	std::cout << RED << "This grade is too high, try within the range 1 - 150" << ENDC << std::endl;
+}
+
+
+void Bureaucrat::GradeTooLowException::message() const
+{
+	std::cout << RED << "This grade is too low, try within the range 1 - 150" << ENDC << std::endl;
+}
+
