98990136373c0a93da72b34f52420be9881887ed
gabrielafreitas <gabriela>
2024-01-23 11:00:02 +0000

    ex00

--- C09/ex00/srcs/main.cpp
+++ C09/ex00/srcs/main.cpp
@@ -10,17 +10,17 @@
 std::time_t getValidDate(std::string vdate)
 {
 	int year = 0, month = 0, day = 0;
-	float value;
+	float value = 0;
 
 	sscanf(vdate.c_str(), "%d-%d-%d,%f", &year, &month, &day, &value);
 	if (year <= 2009 || year > 2024)
-		throw InvalidInputExeception("Invalid year, must be between 2009 and 2024");
+		throw InvalidInputExeception("Invalid year, must be between 2009 and 2024: " + vdate);
 	if (month < 1 || month > 12)
-		throw InvalidInputExeception("Invalid month");
+		throw InvalidInputExeception("Invalid month: " + vdate);
 	if (day < 1 || day > 31)
-		throw InvalidInputExeception("Invalid day");
+		throw InvalidInputExeception("Invalid day: " + vdate);
 	if (value < 1 || value > 1000)
-		throw InvalidInputExeception("Invalid value, must be between 1 and 1000");
+		throw InvalidInputExeception("Invalid value, must be between 1 and 1000: " + vdate);
 
 	std::tm tm;
 	tm.tm_year = year - 1900;
@@ -34,8 +34,8 @@ int main(void)
 	try
 	{
 		std::time_t a, b;
-		a = getValidDate("2024-9-12 ");
-		b = getValidDate("2023-09-11 ");
+		a = getValidDate("2024-9-12,100");
+		b = getValidDate("2023-09-11,90");
 		if (a > b)
 		std::cout << "OK" << std::endl;
 	}
